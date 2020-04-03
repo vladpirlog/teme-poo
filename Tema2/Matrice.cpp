fa9f6144dc52718ca370c0c98892676948417803
Vlad PIRLOG <avi.pirlog@gmail.com>
2020-04-03 21:31:00 +0300

    🔢 Added Matrice class

--- Tema2/Matrice.cpp
+++ Tema2/Matrice.cpp
@@ -2,5 +2,39 @@
 
 Matrice::Matrice()
 {
-    this->v = NULL;
+    this->v = new Complex *[40];
+    for (int i = 0; i < 40; ++i)
+    {
+        (this->v)[i] = new Complex[40];
+    }
+}
+
+Matrice::Matrice(int lin, int col, Complex **v)
+{
+    for (int i = 0; i < lin; ++i)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            (this->v)[i][j] = v[i][j];
+        }
+    }
+}
+
+Matrice::Matrice(const Matrice &m)
+{
+    for (int i = 0; i < 40; ++i)
+    {
+        for (int j = 0; j < 40; j++)
+        {
+            (this->v)[i][j] = (m.v)[i][j];
+        }
+    }
+}
+
+Matrice::~Matrice()
+{
+    for (int i = 0; i < 100; ++i)
+    {
+        delete[](this->v)[i];
+    }
 }
--- Tema2/Matrice.h
+++ Tema2/Matrice.h
@@ -10,17 +10,17 @@ protected:
 
 public:
     Matrice();
-    Matrice(Complex **);
+    Matrice(int, int, Complex **);
     Matrice(const Matrice &);
     ~Matrice();
 
-    virtual Complex at(int, int);
-    virtual void put(Complex, int, int);
+    virtual Complex at(int, int) = 0;
+    virtual void put(Complex, int, int) = 0;
 
-    virtual friend std::istream &operator>>(std::istream &, Matrice &);
-    virtual friend std::ostream &operator<<(std::ostream &, const Matrice &);
+    // virtual friend std::istream &operator>>(std::istream &, Matrice &);
+    // virtual friend std::ostream &operator<<(std::ostream &, const Matrice &);
 
-    virtual void operator=(const Matrice &);
+    // virtual void operator=(const Matrice &);
 };
 
 #endif // MATRICE_H
