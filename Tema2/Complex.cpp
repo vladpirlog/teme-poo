1f3dec15e106bb3c182257c90f59af27c4de96ff
Vlad PIRLOG <avi.pirlog@gmail.com>
2020-04-03 18:18:48 +0300

    © Added Complex class

---
+++ Tema2/Complex.cpp
@@ -0,0 +1,58 @@
+#include "Complex.h"
+
+Complex::Complex()
+{
+    this->re = 0;
+    this->im = 0;
+}
+
+Complex::Complex(float re, float im)
+{
+    this->re = re;
+    this->im = im;
+}
+
+Complex::Complex(const Complex &n)
+{
+    this->re = n.re;
+    this->im = n.im;
+}
+
+Complex::~Complex()
+{
+    std::cout << "Number deleted.\n";
+}
+
+float Complex::getRe()
+{
+    return this->re;
+}
+
+float Complex::getIm()
+{
+    return this->im;
+}
+
+void Complex::operator=(const Complex &x)
+{
+    this->re = x.re;
+    this->im = x.im;
+}
+
+std::istream &operator>>(std::istream &is, Complex &p)
+{
+    float r, i;
+    std::cout << "Enter Re: ";
+    is >> r;
+    p.re = r;
+    std::cout << "Enter Im: ";
+    is >> i;
+    p.im = i;
+
+    return is;
+}
+
+std::ostream &operator<<(std::ostream &, const Complex &p)
+{
+    std::cout << p.re << "+" << p.im << "j\n";
+}
---
+++ Tema2/Complex.h
@@ -0,0 +1,24 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+#include <iostream>
+
+class Complex
+{
+    float re, im;
+
+public:
+    Complex();
+    Complex(float, float);
+    Complex(const Complex &);
+    ~Complex();
+
+    float getRe();
+    float getIm();
+
+    friend std::istream &operator>>(std::istream &, Complex &);
+    friend std::ostream &operator<<(std::ostream &, const Complex &);
+    void operator=(const Complex &);
+};
+
+#endif COMPLEX_H
